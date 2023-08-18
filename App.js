c230fa41443f09192b834b1a6906e6cb99415e15
aristata <aristatait@gmail.com>
2023-08-18 13:59:11 +0900

    feat: 2.4 layout system

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My first React Native App!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+/***************************************************************************************************
+ * 2.4 layout system
+ *
+ * - Flexbox 는 다양한 스크린 크기에서 일관된 레이아웃을 제공하도록 설계되었다
+ * - View 컴포넌트는 기본적으로 Flexbox 이다
+ * - 따로 container 를 지정하지 않아도, 상위 컴포넌트는 container 역할을 수행한다
+ ***************************************************************************************************/
