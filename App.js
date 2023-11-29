728277014f598f81851804daa406257c097d6e50
aristata <aristatait@gmail.com>
2023-11-29 15:04:39 +0900

    feat: Location

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync();
+    // console.log(permission);
+    if (!permission.granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude }
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    // console.log(location);
+    setCity(location[0].city);
+  };
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <View style={styles.city}>
-        <Text style={styles.cityName}>Busan</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         horizontal
