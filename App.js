ab6d284773fed474979941b065b787277b6946a9
aristata <aristatait@gmail.com>
2023-12-08 17:18:43 +0900

    feat: 2.10 icons

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator
+} from "react-native";
 import * as Location from "expo-location";
 import { OPEN_WEATHER_API_KEY } from "@env";
+import { Fontisto } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning"
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
@@ -12,7 +30,7 @@ export default function App() {
   const [ok, setOk] = useState(true);
   const getWeather = async () => {
     const permission = await Location.requestForegroundPermissionsAsync();
-    console.log(permission);
+    // console.log(permission);
     if (!permission.granted) {
       setOk(false);
     }
@@ -23,8 +41,8 @@ export default function App() {
       { latitude, longitude },
       { useGoogleMaps: false }
     );
-    console.log(location);
-    setCity(location[0].city);
+    // console.log(location);
+    setCity(location[0].city ?? location[0].street);
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
     );
@@ -34,12 +52,14 @@ export default function App() {
         return weather;
       }
     });
-    console.log(dayWeathers);
+    // console.log(dayWeathers);
     setDays(dayWeathers);
   };
+
   useEffect(() => {
     getWeather();
   }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -52,15 +72,39 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        {days.map((day) => (
-          <View style={styles.day} key={day.dt}>
-            <Text style={styles.temp}>
-              {parseFloat(day.main.temp).toFixed(1)}
-            </Text>
-            <Text style={styles.description}>{day.weather[0].main}</Text>
-            <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+        {days.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
           </View>
-        ))}
+        ) : (
+          days.map((day) => (
+            <View style={styles.day} key={day.dt}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between"
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.main.temp).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={70}
+                  color="white"
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -69,29 +113,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "yellow"
+    backgroundColor: "tomato"
   },
   city: {
-    flex: 1,
+    flex: 1.2,
     justifyContent: "center",
     alignItems: "center"
   },
   cityName: {
     fontSize: 80,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    color: "white"
   },
-  weather: {
-    backgroundColor: "orange"
-  },
+  weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center"
+    alignItems: "flex-start",
+    paddingHorizontal: 20
   },
   temp: {
     marginTop: 10,
-    fontSize: 120
+    fontSize: 100,
+    color: "white"
   },
   description: {
-    fontSize: 60
+    marginLeft: 10,
+    fontSize: 30,
+    color: "white"
+  },
+  tinyText: {
+    marginTop: -5,
+    marginLeft: 10,
+    fontSize: 25,
+    color: "white"
   }
 });
